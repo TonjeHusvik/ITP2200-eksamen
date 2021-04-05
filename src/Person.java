0402a2be87663a28bb4f3b43014191234acdfba0
runejac <69790920+runejac@users.noreply.github.com>
2021-04-05 14:03:02 +0200

    added constructor for hypercaloricdiet.java and person.java

--- src/Person.java
+++ src/Person.java
@@ -4,7 +4,42 @@ public class Person {
     private Diet diet;
     private float weight;
 
-    public void printPerson(){
-        System.out.println("Hva er din favorittmat?");
+    public Person(Food food, Food[] allergies, Diet diet, float weight) {
+        this.food = food;
+        this.allergies = allergies;
+        this.diet = diet;
+        this.weight = weight;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
+
+    public Food[] getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(Food[] allergies) {
+        this.allergies = allergies;
+    }
+
+    public Diet getDiet() {
+        return diet;
+    }
+
+    public void setDiet(Diet diet) {
+        this.diet = diet;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
     }
 }
