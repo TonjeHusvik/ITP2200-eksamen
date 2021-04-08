963514f9a3007ddd635e00fd067b2bc47c6a5f2f
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-08 19:35:31 +0200

    fixed return statement

--- src/DietManager.java
+++ src/DietManager.java
@@ -2,9 +2,7 @@ public class DietManager {
 
     public boolean areCompatible(Person p, Diet d) {
         //Given a Person and a Diet, return true if they are compatible, false otherwise.
-
-        // Sammenligner Person og Diet
-        return p.getDiet().equals(d.getAllowedFood());
+        return p.getDiet().equals(d.getAllowedFood()); //Sammenligner Person og Diet
     }
 
     public Diet randomDiet(Person p, Food f[]) {
