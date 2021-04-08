e6141480721f2720179bd8a6a16cbbf723ebb705
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-08 15:19:11 +0200

    endret if-else

--- src/DietManager.java
+++ src/DietManager.java
@@ -2,14 +2,9 @@ public class DietManager {
 
     public boolean areCompatible(Person p, Diet d) {
         //Given a Person and a Diet, return true if they are compatible, false otherwise.
-        Person person = new Person();
-        /* Usikker */
 
-        if (person.ge) { // sammenligner Person og Diet
-            return true;
-        }else{
-            return false;
-        }
+        // Sammenligner Person og Diet
+        return p.getDiet().equals(d.getAllowedFood());
     }
 
     public Diet randomDiet(Person p, Food f[]) {
