efbfded74ca8a7618b302f564680ac95f45738eb
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-10 17:09:07 +0200

    Update PersonTest.java

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -1,15 +1,36 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class PersonTest {
-    public void addPerson(){
-        //Person jon = new Person("Meatloaf");
+    ArrayList<Food> allDiets = new ArrayList<>();
+
+    @Before
+    public void tester(){
+        Food random1 = new Food("veganFood", 20, true, FoodType.Fiber);
+        Food random2 = new Food("nonVegan", 20, false, FoodType.Fiber);
+        allDiets.add(random1);
+        allDiets.add(random2);
+
     }
 
-    // TODO a. If their favourite food is nonvegan, they cannot follow a vegan diet.
+    // TODO a. If their favourite food is non-vegan, they cannot follow a vegan diet.
     @Test
-    public void requirement2_a(Person person) {
-        if (!person.getFood().isVegan()){
-            return false;
+    public void requirement2_a() {
+
+        Food[] person1allergies = new Food[1];
+        person1allergies[0] = new Food("Milk", 10, false, FoodType.Fat);
+
+        Person person1 = new Person(new Food("Meatloaf", 100, false, FoodType.Fat),
+                person1allergies,
+                new LowCarbDiet("ss",30,"s", allDiets, false, 50),
+                50);
+
+
+
+        if (!person1.getFood().isVegan()){
+
         }
 
     }
