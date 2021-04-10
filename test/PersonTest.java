24370c3a3c7e05ed437b19524614688c1a1135c0
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-10 14:51:52 +0200

    added method and if else

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -1,10 +1,15 @@
 import org.junit.Test;
 
 public class PersonTest {
+    public void addPerson(){
+        Person jon = new Person("Meatloaf", "Milk", HypercaloricDiet, );
+    }
 
     @Test
-    public void testPerson() {
+    public void testPerson(Person person) {
+        if (!person.getFood().isVegan()){
 
+        }
 
     }
 }
