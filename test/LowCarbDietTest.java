164626672bf534aadccfadedad6d66b29fe0c885
Juliehamun <juam005@student.kristiania.no>
2021-04-10 18:08:06 +0200

    made revrequirement1_b

--- test/LowCarbDietTest.java
+++ test/LowCarbDietTest.java
@@ -8,14 +8,15 @@ import static org.junit.Assert.*;
 
 public class LowCarbDietTest {
     ArrayList <Food> allowedLowCarbFood = new ArrayList<>();
+    ArrayList <Food> allowedLowCarbFoodVegan = new ArrayList<>();
 
 
     @Before
     public void setUp() throws Exception {
         Food lowCarbFood1 = new Food("Cauliflower rice", 23, true, FoodType.Protein);
-        Food lowCarbFood2 = new Food("Salad", 20, false, FoodType.Fiber);
+        Food lowCarbFood2 = new Food("Salad", 20, true, FoodType.Fiber);
         Food lowCarbFood3 = new Food("Tofu", 200, true, FoodType.Protein);
-        Food lowCarbFood4 = new Food("Fish", 130, true, FoodType.Protein);
+        Food lowCarbFood4 = new Food("Fish", 130, false, FoodType.Protein);
         Food lowCarbFood5 = new Food("Broccoli", 20, true, FoodType.Fiber);
         allowedLowCarbFood.add(lowCarbFood1);
         allowedLowCarbFood.add(lowCarbFood2);
@@ -26,6 +27,19 @@ public class LowCarbDietTest {
         Food[] jacksAllergies = new Food[2];
         jacksAllergies[0] = new Food("Apple", 35, true, FoodType.Fiber);
         jacksAllergies[1] = new Food("Chocolate", 500, false, FoodType.Recipe);
+
+        Food lowCarbFoodVegan1 = new Food("Apple", 30, false, FoodType.Fiber);
+        Food lowCarbFoodVegan2 = new Food("Orange", 50, true, FoodType.Fiber);
+        Food lowCarbFoodVegan3 = new Food("Seitan", 200, true, FoodType.Recipe);
+        Food lowCarbFoodVegan4 = new Food("Bread", 400, false, FoodType.Carb);
+        Food lowCarbFoodVegan5 = new Food("Lentils", 250, true, FoodType.Protein);
+        allowedLowCarbFoodVegan.add(lowCarbFoodVegan1);
+        allowedLowCarbFoodVegan.add(lowCarbFoodVegan2);
+        allowedLowCarbFoodVegan.add(lowCarbFoodVegan3);
+        allowedLowCarbFoodVegan.add(lowCarbFoodVegan4);
+        allowedLowCarbFoodVegan.add(lowCarbFoodVegan5);
+
+
 
 
 
@@ -45,6 +59,32 @@ public class LowCarbDietTest {
         //System.out.println("The following food is allowed in this LowCarbDiet: "  + lowCarbD1.writeAllowedFood());
         //assertEquals(lowCarbD1.writeAllowedFood(), allowedLowCarbFood.toString());
     }
+    //TODO a. If a diet contains any non-vegan food, it is considered not vegan (i.e., isVegan = false).
+    @Test
+    public void  requirement1_a(){
+
+        LowCarbDiet lowCarbD1 = new LowCarbDiet("LowcarbDiet", 30, "Health", allowedLowCarbFood, false, 50);
+        for (Food f: lowCarbD1.getAllowedFood()) {
+            if(!f.isVegan()){
+                assertFalse(f.isVegan());
+                System.out.println("not vegan");
+            }
+        }
+    }
+
+    //TODO If a diet contains only vegan food, it is considered vegan, even if it is not a VeganDiet (e.g., it could be a LowCarbDiet).
+    @Test
+    public void  requirement1_b() throws Exception {
+        LowCarbDiet lowCarbD1 = new LowCarbDiet("LowcarbDiet", 30, "Health", allowedLowCarbFoodVegan, false, 50);
+        for (Food f : lowCarbD1.getAllowedFood()) {
+            if (f.isVegan()) {
+                assertTrue(f.isVegan());
+
+            }
+        }
+    }
+
+
 
     @Test
     public void getMinWeightKg() {
