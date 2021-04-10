ea5fb98569a30777c47d5fda2cc0e0721e1ea2af
Joachim AC <joacchri@gmail.com>
2021-04-10 18:45:55 +0100

    LowCarbDietTest req1b done

--- test/LowCarbDietTest.java
+++ test/LowCarbDietTest.java
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> de26919943ec6ed55890e4fe700e47365362829d
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
 
@@ -31,9 +29,9 @@ public class LowCarbDietTest {
         jacksAllergies[0] = new Food("Apple", 35, true, FoodType.Fiber);
         jacksAllergies[1] = new Food("Chocolate", 500, false, FoodType.Recipe);
 
-        Food lowCarbFoodVegan1 = new Food("Apple", 30, false, FoodType.Fiber);
+        Food lowCarbFoodVegan1 = new Food("Apple", 30, true, FoodType.Fiber);
         Food lowCarbFoodVegan2 = new Food("Orange", 50, true, FoodType.Fiber);
-        Food lowCarbFoodVegan3 = new Food("Seitan", 200, true, FoodType.Recipe);
+        Food lowCarbFoodVegan3 = new Food("Seitan", 200, false, FoodType.Recipe);
         Food lowCarbFoodVegan4 = new Food("Bread", 400, false, FoodType.Carb);
         Food lowCarbFoodVegan5 = new Food("Lentils", 250, true, FoodType.Protein);
         allowedLowCarbFoodVegan.add(lowCarbFoodVegan1);
@@ -43,28 +41,15 @@ public class LowCarbDietTest {
         allowedLowCarbFoodVegan.add(lowCarbFoodVegan5);
 
 
-
-
-
-
         Person JACK = new Person(new Food("Chicken", 130, false, FoodType.Protein),
-                jacksAllergies,
-<<<<<<< HEAD
-                               new LowCarbDiet("LowcarbDiet", 30, "Health", allowedLowCarbFood, false, 50),
-=======
-                               new LowCarbDiet("LowCarbDiet",30, "Health", allowedLowCarbFood, false, 50),
->>>>>>> de26919943ec6ed55890e4fe700e47365362829d
+                jacksAllergies, new LowCarbDiet("LowcarbDiet", 30, "Health", allowedLowCarbFood, false, 50),
                                79);
 
         }
 
     @Test
     public void writeAllowedFoodTest(){
-<<<<<<< HEAD
         LowCarbDiet lowCarbD1 = new LowCarbDiet("LowcarbDiet", 30, "Health", allowedLowCarbFood, false, 50);
-=======
-        LowCarbDiet lowCarbD1 = new LowCarbDiet("LowCarbDiet",30, "Health", allowedLowCarbFood, false, 50);
->>>>>>> de26919943ec6ed55890e4fe700e47365362829d
 
         System.out.println(allowedLowCarbFood.toString());
         //System.out.println("The following food is allowed in this LowCarbDiet: "  + lowCarbD1.writeAllowedFood());
@@ -88,10 +73,8 @@ public class LowCarbDietTest {
     public void  requirement1_b() throws Exception {
         LowCarbDiet lowCarbD1 = new LowCarbDiet("LowcarbDiet", 30, "Health", allowedLowCarbFoodVegan, false, 50);
         for (Food f : lowCarbD1.getAllowedFood()) {
-            if (f.isVegan()) {
-                assertTrue(f.isVegan());
-
-            }
+            Assertions.assertAll("Sjekker om alt er vegansk, yo",
+                    () -> assertEquals(true, f.isVegan()));
         }
     }
 
