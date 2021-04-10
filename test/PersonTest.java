9d15b0661d976c2c6ac51d77c90ce54789e8c3aa
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-10 15:01:35 +0200

    fixed things

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -2,13 +2,14 @@ import org.junit.Test;
 
 public class PersonTest {
     public void addPerson(){
-        Person jon = new Person("Meatloaf", "Milk", HypercaloricDiet, );
+        //Person jon = new Person("Meatloaf");
     }
 
+    // TODO a. If their favourite food is nonvegan, they cannot follow a vegan diet.
     @Test
-    public void testPerson(Person person) {
+    public void requirement2_a(Person person) {
         if (!person.getFood().isVegan()){
-
+            return false;
         }
 
     }
