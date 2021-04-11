18f51908996217cd0e1f4e946e266e343ae4e2d6
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-11 12:28:46 +0200

    fixing

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -7,12 +7,15 @@ import static org.junit.Assert.*;
 
 public class PersonTest {
     ArrayList<Food> allowedInVegan = new ArrayList<>();
+    ArrayList<Food> allowedInLowCarb = new ArrayList<>();
 
     @Before
     public void diets(){
-        Food vegFood = new Food("Salad", 10, true, FoodType.Fiber);
-        allowedInVegan.add(vegFood);
-        //VeganDiet veganDiet = new VeganDiet("Vegan diet", 20, "health", allowedInVegan,true,20);
+        Food veganFood1 = new Food("Salad", 10, true, FoodType.Fiber);
+        allowedInVegan.add(veganFood1);
+
+        Food lowCarbFood1 = new Food("Bacon", 10, false, FoodType.Fat);
+        allowedInLowCarb.add(lowCarbFood1);
     }
 
     // TODO a. If their favourite food is non-vegan, they cannot follow a vegan diet.
@@ -20,11 +23,12 @@ public class PersonTest {
     public void requirement2_a() {
         Person person1 = new Person(new Food("Meatloaf", 400, false, FoodType.Fat));
         VeganDiet veganDiet = new VeganDiet("Vegan diet", 20, "health", allowedInVegan,true,20);
+        LowCarbDiet lowCarbDiet = new LowCarbDiet("Low carb diet", 20, "weightloss", allowedInLowCarb, false, 80);
 
         if (!person1.getFood().isVegan() && veganDiet.isVegan()){
             throw new IllegalArgumentException("This person's favouriteFood is " + person1.getFood().getName() + " and cannot follow a vegandiet.");
         }else{
-            System.out.println("does it work?");
+            assertTrue("This person can follow this diet", !person1.getFood().isVegan() && !veganDiet.isVegan());
         }
 
 
