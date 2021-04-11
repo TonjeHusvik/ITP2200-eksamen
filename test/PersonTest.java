9d3102ae326c9f6c7a650cfdb02755f612d1869f
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-11 12:11:15 +0200

    trying to get things to work

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -3,8 +3,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class PersonTest {
     ArrayList<Food> allowedInVegan = new ArrayList<>();
@@ -22,12 +21,30 @@ public class PersonTest {
         Person person1 = new Person(new Food("Meatloaf", 400, false, FoodType.Fat));
         VeganDiet veganDiet = new VeganDiet("Vegan diet", 20, "health", allowedInVegan,true,20);
 
-        assertFalse("bla", !person1.getFood().isVegan() && veganDiet.isVegan());
-
-        //if (!person1.getFood().isVegan() && veganDiet.isVegan()){
-            //System.out.println("This person's favouriteFood is " + person1.getFood().getName() + " and cannot follow a vegan diet.");
-            //throw new IllegalArgumentException("This person's favouriteFood is " + person1.getFood().getName() + " and cannot follow a vegan diet.");
+        if (!person1.getFood().isVegan() && veganDiet.isVegan()){
+            throw new IllegalArgumentException("This person's favouriteFood is " + person1.getFood().getName() + " and cannot follow a vegandiet.");
+        }else{
+            System.out.println("does it work?");
         }
+
+
+        //assertEquals(true, !person1.getFood().isVegan() && veganDiet.isVegan());
+
+        //return !person1.getFood().isVegan() && veganDiet.isVegan();
+
+        //assertTrue(!person1.getFood().isVegan() && veganDiet.isVegan());
+        //assertFalse(person1.getFood().isVegan() && !veganDiet.isVegan());
+
+        //assertTrue("This person's favouriteFood is " + person1.getFood().getName() + " and cannot follow a vegan diet.", !person1.getFood().isVegan() && veganDiet.isVegan());
+        //if (!person1.getFood().isVegan() && veganDiet.isVegan()){
+        // System.out.println("This person's favouriteFood is " + person1.getFood().getName() + " and cannot follow a vegan diet.");
+        //throw new IllegalArgumentException("This person's favouriteFood is " + person1.getFood().getName() + " and cannot follow a vegan diet.");
+        //}
+
+    }
+
+    @Test
+    public void requirement2_b(){
 
     }
 }
