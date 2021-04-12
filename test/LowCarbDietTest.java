c5be2ce1f41808eaeb4a2572656fda61c6d49525
runejac <69790920+runejac@users.noreply.github.com>
2021-04-12 12:28:21 +0200

    hey

--- test/LowCarbDietTest.java
+++ test/LowCarbDietTest.java
@@ -17,7 +17,7 @@ public class LowCarbDietTest {
         Food lowCarbFood1 = new Food("Cauliflower rice", 23, true, FoodType.Protein);
         Food lowCarbFood2 = new Food("Salad", 20, true, FoodType.Fiber);
         Food lowCarbFood3 = new Food("Tofu", 200, true, FoodType.Protein);
-        Food lowCarbFood4 = new Food("Fish", 130, false, FoodType.Protein);
+        Food lowCarbFood4 = new Food("Fish", 130, true, FoodType.Protein);
         Food lowCarbFood5 = new Food("Broccoli", 20, true, FoodType.Fiber);
         allowedLowCarbFood.add(lowCarbFood1);
         allowedLowCarbFood.add(lowCarbFood2);
