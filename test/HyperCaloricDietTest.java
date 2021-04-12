9e8be5561a53a055945381f15bb4504b7a1af30e
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-12 19:11:10 +0200

    hypercaloric

---
+++ test/HyperCaloricDietTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+/*
+    public void dietRestriction1b(Diet d) {
+        for (Food f : d.getAllowedFood()) {
+            if (f.isVegan()) {
+                d.isVegan = true;
+            }
+        }
+    }
+    */
+
+public class HyperCaloricDietTest {
+    //TODO Denne sjekker at dietten inneholder vegansk mat og skal sjekke at d.isVegan blir til true
+    @Test
+    //Teste en diett hvor alt er vegan
+    public void requirement1b_1(){
+    Diet d = new Diet();
+
+    }
+
+    @Test
+    //Teste en diett hvor en eller fler ikke er vegan
+    public void requirement1b_2(){
+
+    }
+
+    @Test
+    //Test der ingen er vegan?
+    public void requirement1b_3(){
+
+    }
+
+    @Test
+    public void requirement1b_4(){
+
+    }
+
+}
