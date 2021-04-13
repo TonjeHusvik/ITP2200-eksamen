fbea40b5cd448df5ea5e9955abca3265f4dbba2a
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-13 12:29:43 +0200

    d

--- test/HyperCaloricDietTest.java
+++ test/HyperCaloricDietTest.java
@@ -1,7 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
-import org.junit.jupiter.api.Assertions;
 import static org.junit.Assert.*;
 
 public class HyperCaloricDietTest {
