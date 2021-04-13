40e3a8fd06f5c0aad4c91af4b3b479bcbbf1b349
runejac <69790920+runejac@users.noreply.github.com>
2021-04-13 15:29:33 +0200

    3b

--- test/VeganDietTest.java
+++ test/VeganDietTest.java
@@ -62,28 +62,7 @@ public class VeganDietTest {
     }
 */
 
-    // TODO Write the duration of a diet in terms of years, months and days, eg., "This VeganDiet lasts for 2 years, 3 months and 5 days".
-    /*@Test
-    public void requirement3_a() {
-        VeganDiet veganDiet1 = new VeganDiet("VeganDiet", 400, "Save the planet!", allowedInVegan, true, 2);
-
-        String result = veganDiet1.writeDuration(veganDiet1);
-        assertEquals("This VeganDiet lasts for 1 years, 1 months and 5 days", result);
-    }*/
-
-    // TODO Write the allowed food, e.g., "The following food is allowed in this FlexitarianDiet: Salad, Soup, Apple, Strawberry, Salmon".
-   /* @Test
-    public void requirement3_b() {
-        VeganDiet veganDiet1 = new VeganDiet("VeganDiet", 70, "Save the earth.", allowedInVegan, true, 50);
-
-        String result = veganDiet1.writeAllowedFood(veganDiet1)
-                        .replace("[",  "")
-                        .replace("]", "");
-        assertEquals("The following food is allowed in this VeganDiet: Rice, Salad, Tofu, Beans, Broccoli", result);
-        System.out.println(result);
-    }
-
-    // TODO Given a Person and a Diet, return true if they are compatible, false otherwise
+/*    // TODO Given a Person and a Diet, return true if they are compatible, false otherwise
     @Test
     public void requirement4_a() {
         Food[] person1Allergies = new Food[2];
@@ -96,5 +75,5 @@ public class VeganDietTest {
         DietManager dietManager = new DietManager();
 
         assertTrue(dietManager.areCompatible(person1, person1VeganDiet));
-    }
-*/
+    }*/
+
