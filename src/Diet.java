6b61748eb86db8016a401b8e011d9af5f26b6dec
TonjeHusvik <tonje.husvik@hotmail.com>
2021-04-13 12:33:55 +0200

    Testing is correct and finished

--- src/Diet.java
+++ src/Diet.java
@@ -102,12 +102,17 @@ public abstract class Diet {
     Kanskje fler tester?
 
      */
-    public void dietRestriction1b(Diet d) {
-        for (Food f : d.getAllowedFood()) {
+    public boolean dietRestriction1b() {
+        for (Food f : getAllowedFood()) {
             if (f.isVegan()) {
-                d.isVegan = true;
+                //isVegan = true;
+                return true;
+            }else{
+                //isVegan = false;
+                return false;
             }
         }
+        return false;
     }
 
     /* Joachim - A VeganDiet cannot contain non-vegan food.
