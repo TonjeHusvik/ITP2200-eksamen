e53635627379ec75da7865868bc1e9061ce2563a
runejac <69790920+runejac@users.noreply.github.com>
2021-04-14 18:42:51 +0200

    fixed some small t hings in req1b

--- test/HyperCaloricDietTest.java
+++ test/HyperCaloricDietTest.java
@@ -16,11 +16,11 @@ public class HyperCaloricDietTest {
         allowedInHyperCaloric.add(hyperCaloric2);
 
         Food flexFood1 = new Food("Beef", 20, false, FoodType.Fiber);
-        Food flexFood2 = new Food("Nuts", 20, false, FoodType.Fiber);
+        Food flexFood2 = new Food("Fish", 20, false, FoodType.Fiber);
         allowedInFlexiDiet.add(flexFood1);
         allowedInFlexiDiet.add(flexFood2);
 
-        Food lowCarb1 = new Food("Nuts", 20, false, FoodType.Fiber);
+        Food lowCarb1 = new Food("Beef", 20, false, FoodType.Fiber);
         Food lowCarb2 = new Food("Nuts", 20, true, FoodType.Fiber);
         allowedInLowCarb.add(lowCarb1);
         allowedInLowCarb.add(lowCarb2);
@@ -29,25 +29,30 @@ public class HyperCaloricDietTest {
     //TODO If a diet contains only vegan food, it is considered vegan, even if it is not a VeganDiet
     // (e.g., it could be a LowCarbDiet).
 
-    @Test //Teste en diett hvor alt er vegan
+
+    // Test a diet that is not from the VeganDiet, but is fully vegan
+    // PASSED✔️
+    @Test
     public void requirement1b_1(){
-        HypercaloricDiet hypercaloricDiet = new HypercaloricDiet("Hyper caloric diet", 60, "health", allowedInHyperCaloric, false, 120, 600);
-        assertTrue("The food in this diet is vegan",hypercaloricDiet.dietRestriction1b());
+        HypercaloricDiet hypercaloricDiet = new HypercaloricDiet("Hyper caloric diet", 60, "health", allowedInHyperCaloric, true, 120, 600);
+        assertTrue("The food in this diet is not vegan",hypercaloricDiet.dietRestriction1b());
     }
 
+    // Test a diet where parts of it is vegan
+    // PASSED✔️
     @Test
-    //Teste en diett hvor en eller fler ikke er vegan
     public void requirement1b_2(){
         LowCarbDiet lowCarbDiet = new LowCarbDiet("Low carb diet", 60, "health", allowedInLowCarb, false, 40);
-        assertFalse("One or more dishes in this diet is not vegan", lowCarbDiet.dietRestriction1b());
+        assertFalse("The food in this diet is vegan", lowCarbDiet.dietRestriction1b());
     }
 
+    // Test a diet where nothing is vegan
+    // PASSED✔
     @Test
-    //Teste en diett hvor ingenting er vegansk
     public void requirement1b_3(){
         Food flexFood1 = new Food("Beef", 20, false, FoodType.Fiber);
         FlexitarianDiet flexitarianDiet = new FlexitarianDiet("Low carb diet", 60, "health",
                 allowedInFlexiDiet, false, 400, flexFood1);
-        assertFalse("Nothing in this diet is vegan", flexitarianDiet.dietRestriction1b());
+        assertFalse("Everything in this diet is vegan", flexitarianDiet.dietRestriction1b());
     }
 }
