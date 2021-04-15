26e771edf5b68722d4c16fbbf9b0d66f9d8c9b9b
Juliehamun <juam005@student.kristiania.no>
2021-04-15 12:28:10 +0200

    update

--- test/PersonTest.java
+++ test/PersonTest.java
@@ -1,29 +1,36 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-
 import java.util.ArrayList;
-
 import static org.junit.Assert.*;
+
+// PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔
+// PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔
+// PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔
 
 public class PersonTest {
 
     ArrayList<Food> allowedInVegan = new ArrayList<>();
     ArrayList<Food> allowedInLowCarb = new ArrayList<>();
     ArrayList<Food> allowedInHyperCaloric = new ArrayList<>();
+    ArrayList<Food> allergiesTooMany = new ArrayList<>();
+    ArrayList<Food> allergiesOk = new ArrayList<>();
+    ArrayList<Food> allowedInDiet = new ArrayList<>();
+
 
     @Before
     public void diets(){
 
+        // VEGAN FOOD
         Food veganFood1 = new Food("Salad", 10, true, FoodType.Fiber);
         Food veganFood2 = new Food("Apple", 30, true, FoodType.Fat);
-        Food veganFood3= new Food("Orange", 50, true, FoodType.Fiber);
+        Food veganFood3 = new Food("Orange", 50, true, FoodType.Fiber);
         Food veganFood4 = new Food("Seitan", 200, true, FoodType.Carb);
         allowedInVegan.add(veganFood1);
         allowedInVegan.add(veganFood2);
-        allowedInVegan.add(veganFood2);
-        allowedInVegan.add(veganFood2);
+        allowedInVegan.add(veganFood3);
+        allowedInVegan.add(veganFood4);
 
+        // LOW CARB FOOD
         Food lowCarbFood1 = new Food("Bacon", 10, false, FoodType.Fat);
         Food lowCarbFood2 = new Food("Pork", 10, false, FoodType.Fat);
         Food lowCarbFood3 = new Food("Peanuts", 10, false, FoodType.Fat);
@@ -35,6 +42,7 @@ public class PersonTest {
         allowedInLowCarb.add(lowCarbFood4);
         allowedInLowCarb.add(lowCarbFood5);
 
+        // HYPERCALORIC FOOD
         Food hyperCaloricFood1 = new Food("Bacon", 10, false, FoodType.Fat);
         Food hyperCaloricFood2 = new Food("Butter", 10, false, FoodType.Fat);
         Food hyperCaloricFood3 = new Food("Peanut butter", 10, false, FoodType.Fat);
@@ -44,6 +52,32 @@ public class PersonTest {
         allowedInHyperCaloric.add(hyperCaloricFood3);
         allowedInHyperCaloric.add(hyperCaloricFood4);
 
+        // FLEXITARIAN FOOD
+        Food flexiFood1 = new Food("Salad", 10, true, FoodType.Fiber);
+        Food flexiFood2 = new Food("Crab", 10, false, FoodType.Protein);
+        Food flexiFood3 = new Food("Beef", 10, false, FoodType.Protein);
+        Food flexiFood4 = new Food("Pomegranate", 10, true, FoodType.Fiber);
+        Food flexiFood5 = new Food("Peanuts", 10, true, FoodType.Fat);
+        Food flexiFood6 = new Food("Wheat", 10, true, FoodType.Fiber);
+        allowedInDiet.add(flexiFood1);
+        allowedInDiet.add(flexiFood2);
+        allowedInDiet.add(flexiFood3);
+        allowedInDiet.add(flexiFood4);
+        allowedInDiet.add(flexiFood5);
+        allowedInDiet.add(flexiFood6);
+
+        // ALLERGY FOOD
+        Food allergyFood1 = new Food("Spinach", 10, true, FoodType.Fiber);
+        Food allergyFood2 = new Food("Asparagus", 10, false, FoodType.Fiber);
+        Food allergyFood3 = new Food("Salmon", 10, false, FoodType.Protein);
+        allergiesTooMany.add(allergyFood3);
+        allergiesTooMany.add(flexiFood4);
+        allergiesTooMany.add(flexiFood5);
+        allergiesTooMany.add(flexiFood6);
+        allergiesOk.add(allergyFood1);
+        allergiesOk.add(allergyFood2);
+        allergiesOk.add(allergyFood3);
+        allergiesOk.add(flexiFood4);
     }
 
 
@@ -76,6 +110,27 @@ public class PersonTest {
     /*-----------------------------------------------------------------------------------------------------------------*/
     /*-----------------------------------------------------------------------------------------------------------------*/
 
+    /***Requirement 2.b: They cannot follow a diet if they are allergic to 50% or more of the food
+     allowed by the diet.***/
+    //Testing with too many allergies
+    //PASSED ✔️
+    @Test (expected = IllegalArgumentException.class)
+    public void requirement2_b() {
+        Person tooManyAllergies = new Person(allergiesTooMany);
+        FlexitarianDiet flexiDiet = new FlexitarianDiet("Flexi", 324, "testing", allowedInDiet,
+                false, 1000, (new Food("Tuna", 400, false, FoodType.Protein)));
+        tooManyAllergies.personRestriction2b(tooManyAllergies, flexiDiet);
+    }
+
+    //Testing with less than 50% allergies
+    //PASSED ✔️
+    @Test
+    public void requirement2_b_1() {
+        Person okAmountOfAllergies = new Person(allergiesOk);
+        FlexitarianDiet flexiDiet = new FlexitarianDiet("Flexi", 324, "testing", allowedInDiet,
+                false, 1000, (new Food("Tuna", 400, false, FoodType.Protein)));
+        assertTrue(okAmountOfAllergies.personRestriction2b(okAmountOfAllergies, flexiDiet));
+    }
 
     /*--------------------------------------------------------------------------------------------------------------------*/
     /*--------------------------------------------------------------------------------------------------------------------*/
--- test/RandomDietTest.java
+++ test/RandomDietTest.java
@@ -1,39 +1,52 @@
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.ArrayList;
 
-public class randomDiet4bTest {
+// PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔
+// PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔
+// PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔PASSED✔
+
+
+public class RandomDietTest {
     ArrayList<Food> allergies = new ArrayList<>();
     ArrayList<Food> food = new ArrayList<>();
     Person randomPerson = new Person(new Food("Salad", 10, true, FoodType.Fiber), allergies, 80);
 
     @Before
     public void setUp() {
+        // FOOD ARRAY
         Food food1 = new Food("Bacon", 10, false, FoodType.Fat);
         Food food2 = new Food("Pork", 10, false, FoodType.Protein);
         Food food3 = new Food("Peanuts", 10, true, FoodType.Fat);
+        food.add(food1);
+        food.add(food2);
+        food.add(food3);
 
+        // ALLERGY FOOD
         Food allergyFood1 = new Food("Asparagus", 10, true, FoodType.Fiber);
         Food allergyFood2 = new Food("Butter", 10, false, FoodType.Fat);
         Food allergyFood3 = new Food("Peanut butter", 10, true, FoodType.Fat);
         Food allergyFood4 = new Food("Lard", 10, false, FoodType.Fat);
-
         allergies.add(allergyFood1);
         allergies.add(allergyFood2);
         allergies.add(allergyFood3);
         allergies.add(allergyFood4);
-
-        food.add(food1);
-        food.add(food2);
-        food.add(food3);
         food.add(allergyFood1);
         food.add(allergyFood3);
     }
 
-    //TODO: No assertions here, because we're only testing that the method works
+    /*** Requirement 4. b:
+     * Given a Person and a list of Food, create a random HypercaloricDiet with the following attributes:
+     i. daysDuration: random number between 1 and 100.
+     ii. purpose: “Random diet”.
+     iii. allowedFood: all the Food from the list that the person is not allergic to.
+     iv. isVegan: false if there is some non-vegan Food, true otherwise.
+     v. maxWeightKg: random number between Person.weight and Person.weight + 20.
+     vi. minCaloriesPerDay: random number between 2000 and 4000.***/
+
+    // PASSED✔
     @Test
-    public void randomDiet4b() {
+    public void randomDiet4_b() {
         DietManager randomDietManager = new DietManager();
         System.out.println(randomDietManager.randomDiet(randomPerson, food));
     }
