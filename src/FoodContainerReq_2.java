b07bfd6568f8e4bcdb835a78bb44ba644fe084a1
runejac <69790920+runejac@users.noreply.github.com>
2021-04-17 19:44:58 +0200

    added reqcontainer

---
+++ src/FoodContainerReq_2.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+
+public class FoodContainerReq_2 {
+
+    public ArrayList<Food> allowedInVegan() {
+        ArrayList<Food> allowedInVegan = new ArrayList<>();
+        Food veganFood1 = new Food("Salad", 10, true, FoodType.FIBER);
+        Food veganFood2 = new Food("Apple", 30, true, FoodType.FAT);
+        Food veganFood3 = new Food("Orange", 50, true, FoodType.FIBER);
+        Food veganFood4 = new Food("Seitan", 200, true, FoodType.CARB);
+        allowedInVegan.add(veganFood1);
+        allowedInVegan.add(veganFood2);
+        allowedInVegan.add(veganFood3);
+        allowedInVegan.add(veganFood4);
+        return allowedInVegan;
+    }
+
+    public ArrayList<Food> allowedInLowCarb() {
+        ArrayList<Food> allowedInLowCarb = new ArrayList<>();
+        Food lowCarbFood1 = new Food("Bacon", 541, false, FoodType.FAT);
+        Food lowCarbFood2 = new Food("Pork", 242, false, FoodType.FAT);
+        Food lowCarbFood3 = new Food("Peanuts", 567, false, FoodType.FAT);
+        Food lowCarbFood4 = new Food("Shellfish", 99, false, FoodType.PROTEIN);
+        Food lowCarbFood5 = new Food("Potatoes", 307, false, FoodType.CARB);
+        allowedInLowCarb.add(lowCarbFood1);
+        allowedInLowCarb.add(lowCarbFood2);
+        allowedInLowCarb.add(lowCarbFood3);
+        allowedInLowCarb.add(lowCarbFood4);
+        allowedInLowCarb.add(lowCarbFood5);
+        return allowedInLowCarb;
+    }
+
+    public ArrayList<Food> allowedInHyperCaloric() {
+        ArrayList<Food> allowedInHyperCaloric = new ArrayList<>();
+        Food hyperCaloricFood1 = new Food("Bacon", 541, false, FoodType.FAT);
+        Food hyperCaloricFood2 = new Food("Butter", 716, false, FoodType.FAT);
+        Food hyperCaloricFood3 = new Food("Peanut butter", 588, false, FoodType.FAT);
+        Food hyperCaloricFood4 = new Food("Lard", 898, false, FoodType.FAT);
+        allowedInHyperCaloric.add(hyperCaloricFood1);
+        allowedInHyperCaloric.add(hyperCaloricFood2);
+        allowedInHyperCaloric.add(hyperCaloricFood3);
+        allowedInHyperCaloric.add(hyperCaloricFood4);
+        return allowedInHyperCaloric;
+    }
+
+    public ArrayList<Food> allowedInDiet() {
+        ArrayList<Food> allowedInDiet = new ArrayList<>();
+        Food flexiFood1 = new Food("Salad", 10, true, FoodType.FIBER);
+        Food flexiFood2 = new Food("Crab", 82, false, FoodType.PROTEIN);
+        Food flexiFood3 = new Food("Beef", 250, false, FoodType.PROTEIN);
+        Food flexiFood4 = new Food("Pomegranate", 234, true, FoodType.FIBER);
+        Food flexiFood5 = new Food("Peanuts", 567, true, FoodType.FAT);
+        Food flexiFood6 = new Food("Wheat", 315, true, FoodType.CARB);
+        allowedInDiet.add(flexiFood1);
+        allowedInDiet.add(flexiFood2);
+        allowedInDiet.add(flexiFood3);
+        allowedInDiet.add(flexiFood4);
+        allowedInDiet.add(flexiFood5);
+        allowedInDiet.add(flexiFood6);
+        return allowedInDiet;
+    }
+
+    public ArrayList<Food> allergiesTooMany() {
+        ArrayList<Food> allergiesTooMany = new ArrayList<>();
+        Food flexiFood4 = new Food("Pomegranate", 234, true, FoodType.FIBER);
+        Food flexiFood5 = new Food("Peanuts", 567, true, FoodType.FAT);
+        Food flexiFood6 = new Food("Wheat", 315, true, FoodType.CARB);
+        Food allergyFood3 = new Food("Salmon", 208, false, FoodType.PROTEIN);
+        allergiesTooMany.add(flexiFood4);
+        allergiesTooMany.add(flexiFood5);
+        allergiesTooMany.add(flexiFood6);
+        allergiesTooMany.add(allergyFood3);
+        return allergiesTooMany;
+    }
+
+    public ArrayList<Food> allergiesOk() {
+        ArrayList<Food> allergiesOk = new ArrayList<>();
+        Food allergyFood1 = new Food("Spinach", 23, true, FoodType.FIBER);
+        Food allergyFood2 = new Food("Asparagus", 20, false, FoodType.FIBER);
+        Food allergyFood3 = new Food("Salmon", 208, false, FoodType.PROTEIN);
+        Food flexiFood4 = new Food("Pomegranate", 234, true, FoodType.FIBER);
+        allergiesOk.add(allergyFood1);
+        allergiesOk.add(allergyFood2);
+        allergiesOk.add(allergyFood3);
+        allergiesOk.add(flexiFood4);
+        return allergiesOk;
+    }
+}
--- test/Req_2_Test.java
+++ test/Req_2_Test.java
@@ -9,76 +9,7 @@ import static org.junit.Assert.assertTrue;
 
 public class Req_2_Test {
 
-    ArrayList<Food> allowedInVegan = new ArrayList<>();
-    ArrayList<Food> allowedInLowCarb = new ArrayList<>();
-    ArrayList<Food> allowedInHyperCaloric = new ArrayList<>();
-    ArrayList<Food> allergiesTooMany = new ArrayList<>();
-    ArrayList<Food> allergiesOk = new ArrayList<>();
-    ArrayList<Food> allowedInDiet = new ArrayList<>();
-
-    @Before
-    public void diets(){
-
-        // VEGAN FOOD
-        Food veganFood1 = new Food("Salad", 10, true, FoodType.FIBER);
-        Food veganFood2 = new Food("Apple", 30, true, FoodType.FAT);
-        Food veganFood3 = new Food("Orange", 50, true, FoodType.FIBER);
-        Food veganFood4 = new Food("Seitan", 200, true, FoodType.CARB);
-        allowedInVegan.add(veganFood1);
-        allowedInVegan.add(veganFood2);
-        allowedInVegan.add(veganFood3);
-        allowedInVegan.add(veganFood4);
-
-        // LOW CARB FOOD
-        Food lowCarbFood1 = new Food("Bacon", 541, false, FoodType.FAT);
-        Food lowCarbFood2 = new Food("Pork", 242, false, FoodType.FAT);
-        Food lowCarbFood3 = new Food("Peanuts", 567, false, FoodType.FAT);
-        Food lowCarbFood4 = new Food("Shellfish", 99, false, FoodType.PROTEIN);
-        Food lowCarbFood5 = new Food("Potatoes", 307, false, FoodType.CARB);
-        allowedInLowCarb.add(lowCarbFood1);
-        allowedInLowCarb.add(lowCarbFood2);
-        allowedInLowCarb.add(lowCarbFood3);
-        allowedInLowCarb.add(lowCarbFood4);
-        allowedInLowCarb.add(lowCarbFood5);
-
-        // HYPERCALORIC FOOD
-        Food hyperCaloricFood1 = new Food("Bacon", 541, false, FoodType.FAT);
-        Food hyperCaloricFood2 = new Food("Butter", 716, false, FoodType.FAT);
-        Food hyperCaloricFood3 = new Food("Peanut butter", 588, false, FoodType.FAT);
-        Food hyperCaloricFood4 = new Food("Lard", 898, false, FoodType.FAT);
-        allowedInHyperCaloric.add(hyperCaloricFood1);
-        allowedInHyperCaloric.add(hyperCaloricFood2);
-        allowedInHyperCaloric.add(hyperCaloricFood3);
-        allowedInHyperCaloric.add(hyperCaloricFood4);
-
-        // FLEXITARIAN FOOD
-        Food flexiFood1 = new Food("Salad", 10, true, FoodType.FIBER);
-        Food flexiFood2 = new Food("Crab", 82, false, FoodType.PROTEIN);
-        Food flexiFood3 = new Food("Beef", 250, false, FoodType.PROTEIN);
-        Food flexiFood4 = new Food("Pomegranate", 234, true, FoodType.FIBER);
-        Food flexiFood5 = new Food("Peanuts", 567, true, FoodType.FAT);
-        Food flexiFood6 = new Food("Wheat", 315, true, FoodType.CARB);
-        allowedInDiet.add(flexiFood1);
-        allowedInDiet.add(flexiFood2);
-        allowedInDiet.add(flexiFood3);
-        allowedInDiet.add(flexiFood4);
-        allowedInDiet.add(flexiFood5);
-        allowedInDiet.add(flexiFood6);
-
-        // ALLERGY FOOD
-        Food allergyFood1 = new Food("Spinach", 23, true, FoodType.FIBER);
-        Food allergyFood2 = new Food("Asparagus", 20, false, FoodType.FIBER);
-        Food allergyFood3 = new Food("Salmon", 208, false, FoodType.PROTEIN);
-        allergiesTooMany.add(allergyFood3);
-        allergiesTooMany.add(flexiFood4);
-        allergiesTooMany.add(flexiFood5);
-        allergiesTooMany.add(flexiFood6);
-        allergiesOk.add(allergyFood1);
-        allergiesOk.add(allergyFood2);
-        allergiesOk.add(allergyFood3);
-        allergiesOk.add(flexiFood4);
-    }
-
+    FoodContainerReq_2 foodContainerReq2 = new FoodContainerReq_2();
 
     /*** Requirement 2.a: If their favorite food is non-vegan, they cannot follow a VeganDiet ***/
 
@@ -86,7 +17,7 @@ public class Req_2_Test {
     // PASSED✔
     @Test(expected = IllegalArgumentException.class)
     public void requirement2a() {
-        VeganDiet veganDiet = new VeganDiet("Vegan diet", 20, "health", allowedInVegan,true,20);
+        VeganDiet veganDiet = new VeganDiet("Vegan diet", 20, "health", foodContainerReq2.allowedInVegan(), true,20);
         Person personNotVeganFavorite = new Person(new Food("Tuna", 400, false, FoodType.PROTEIN),
                 veganDiet);
 
@@ -97,7 +28,7 @@ public class Req_2_Test {
     // PASSED✔
     @Test
     public void requirement2a_1() {
-        VeganDiet veganDiet = new VeganDiet("Vegan diet", 20, "health", allowedInVegan,true,20);
+        VeganDiet veganDiet = new VeganDiet("Vegan diet", 20, "health", foodContainerReq2.allowedInVegan(), true,20);
         Person personVeganFavorite = new Person(new Food("Smoked tofu", 140, true, FoodType.PROTEIN),
                 veganDiet);
 
@@ -116,8 +47,8 @@ public class Req_2_Test {
     //PASSED ✔️
     @Test (expected = IllegalArgumentException.class)
     public void requirement2b() {
-        Person tooManyAllergies = new Person(allergiesTooMany);
-        FlexitarianDiet flexiDiet = new FlexitarianDiet("Flexi", 324, "testing", allowedInDiet,
+        Person tooManyAllergies = new Person(foodContainerReq2.allergiesTooMany());
+        FlexitarianDiet flexiDiet = new FlexitarianDiet("Flexi", 324, "testing", foodContainerReq2.allowedInDiet(),
                 false, 1000, (new Food("Tuna", 129, false, FoodType.PROTEIN)));
         tooManyAllergies.personRestriction2b(tooManyAllergies, flexiDiet);
     }
@@ -126,8 +57,8 @@ public class Req_2_Test {
     //PASSED ✔️
     @Test
     public void requirement2b_1() {
-        Person okAmountOfAllergies = new Person(allergiesOk);
-        FlexitarianDiet flexiDiet = new FlexitarianDiet("Flexi", 324, "testing", allowedInDiet,
+        Person okAmountOfAllergies = new Person(foodContainerReq2.allergiesOk());
+        FlexitarianDiet flexiDiet = new FlexitarianDiet("Flexi", 324, "testing", foodContainerReq2.allowedInDiet(),
                 false, 1000, (new Food("Tuna", 129, false, FoodType.PROTEIN)));
         assertTrue(okAmountOfAllergies.personRestriction2b(okAmountOfAllergies, flexiDiet));
     }
@@ -147,7 +78,7 @@ public class Req_2_Test {
         ArrayList<Food> pAllergies = new ArrayList<>();
         Food apple = new Food("Apple", 35, true, FoodType.FIBER);
         pAllergies.add(apple);
-        VeganDiet vd = new VeganDiet("Vegan diet", 200, "Save the planet", allowedInVegan, true, 50);
+        VeganDiet vd = new VeganDiet("Vegan diet", 200, "Save the planet", foodContainerReq2.allowedInVegan(), true, 50);
 
         Person p = new Person(new Food("Tofu", 76, true, FoodType.PROTEIN), pAllergies,
                 vd , 80);
@@ -162,7 +93,7 @@ public class Req_2_Test {
         ArrayList<Food> pAllergies = new ArrayList<>();
         Food apple = new Food("Apple", 35, true, FoodType.FIBER);
         pAllergies.add(apple);
-        VeganDiet vd = new VeganDiet("Vegan diet", 200, "Save the planet", allowedInVegan, true, 50);
+        VeganDiet vd = new VeganDiet("Vegan diet", 200, "Save the planet", foodContainerReq2.allowedInVegan(), true, 50);
 
         Person p = new Person(new Food("Tofu", 76, true, FoodType.PROTEIN), pAllergies,
                 vd , 45);
@@ -177,7 +108,7 @@ public class Req_2_Test {
         ArrayList<Food> pAllergies = new ArrayList<>();
         Food peanuts = new Food("Peanuts", 450, true, FoodType.PROTEIN);
         pAllergies.add(peanuts);
-        LowCarbDiet lcd = new LowCarbDiet("Lowcarb Diet", 200, "Burn fat", allowedInLowCarb, false, 50);
+        LowCarbDiet lcd = new LowCarbDiet("Lowcarb Diet", 200, "Burn fat", foodContainerReq2.allowedInLowCarb(), false, 50);
 
         Person p = new Person(new Food("Pizza", 550, false, FoodType.RECIPE), pAllergies,
                 lcd , 63);
@@ -192,7 +123,7 @@ public class Req_2_Test {
         ArrayList<Food> pAllergies = new ArrayList<>();
         Food peanuts = new Food("Peanuts", 450, true, FoodType.PROTEIN);
         pAllergies.add(peanuts);
-        LowCarbDiet lcd = new LowCarbDiet("Lowcarb Diet", 200, "Burn fat", allowedInLowCarb, false, 50);
+        LowCarbDiet lcd = new LowCarbDiet("Lowcarb Diet", 200, "Burn fat", foodContainerReq2.allowedInLowCarb(), false, 50);
         Person p = new Person(new Food("Pizza", 550, false, FoodType.RECIPE), pAllergies, lcd , 43);
 
         p.personRestriction2c_2(lcd);
@@ -210,7 +141,7 @@ public class Req_2_Test {
     @Test
     public void requirement2d() {
         HypercaloricDiet hypercaloricDiet = new HypercaloricDiet("Hypercaloric diet", 123, "weight gain",
-                allowedInHyperCaloric, false, 100, 6000);
+                foodContainerReq2.allowedInHyperCaloric(), false, 100, 6000);
         Person highWeightPerson = new Person(hypercaloricDiet, 90);
 
         assertTrue(highWeightPerson.personRestriction2_d(hypercaloricDiet));
@@ -221,7 +152,7 @@ public class Req_2_Test {
     @Test (expected = IllegalArgumentException.class)
     public void requirement2d_1() {
         HypercaloricDiet hypercaloricDiet = new HypercaloricDiet("Hypercaloric diet", 123, "weight gain",
-                allowedInHyperCaloric, false, 100, 6000);
+                foodContainerReq2.allowedInHyperCaloric(), false, 100, 6000);
         Person highWeightPerson = new Person(hypercaloricDiet, 120);
 
         highWeightPerson.personRestriction2_d(hypercaloricDiet);
